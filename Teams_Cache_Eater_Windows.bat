4669078558a3954b4a2bb9ce1d41b1cb974a5014
Abhigyan Kumar <51279790+nitianabhigyan@users.noreply.github.com>
2022-02-05 00:12:17 +0530

    Adding script for

---
+++ Teams_Cache_Eater_Windows.bat
@@ -0,0 +1,15 @@
+@echo off
+echo "Killing teams 🔪"
+taskkill /f /im Teams.exe
+echo "Killing Outlook 🔪"
+taskkill /f /im outlook.exe
+timeout 1
+echo "changing directories"
+cd %appdata%\Microsoft
+rd /s /q "./Teams"
+echo "Cache removed"
+echo "Initiating restart"
+start /MAX /B %localappdata%\Microsoft\Teams\Update.exe --processStart "Teams.exe"
+start outlook.exe
+echo My job here is done. :) (Don't forget turn dark mode on if you prefer that in teams :P)
+timeout 5
