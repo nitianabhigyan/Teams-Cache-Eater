908423386e08e680f9702e0f9a42945b61774e80
Abhigyan Kumar <51279790+nitianabhigyan@users.noreply.github.com>
2022-02-07 16:31:14 +0530

    Updates Profile pics without resetting dark mode
    working version.

---
+++ Teams_Cache_Eater_MacOS.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+# Kill, Clean, Start as simple as that.
+# author: Abhigyan Kumar
+# GitHub: www.github.com/nitianabhigyan
+
+echo "\n\e[40;38;5;82mStarting \e[30;48;5;82m Script!\e[0m"
+echo "Killing 🔪Microsoft Teams🗡️" 
+pkill "Microsoft Teams"
+
+echo "\e[92mStarting Cache clean up \e[0m"
+rm -rf ~/Library/Application\ Support/Microsoft/Teams/Cache/*
+echo "\e[92mAttempting Microsoft Teams restart\e[0m"
+sleep 3
+open "/Applications/Microsoft Teams.app"
+echo "\e[40;38;5;82mMy job here is done. :-)\e[0m"
+echo "\e[93m\e[41mDon't forget to change back to dark mode (if you had it turned on earlier)\e[0m\n" 
